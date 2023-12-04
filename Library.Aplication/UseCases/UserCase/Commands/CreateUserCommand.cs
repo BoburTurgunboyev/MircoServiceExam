b4e4274a90111241219981dcd2e0fa7b40e604b5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:38 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/UserCase/Commands/CreateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using Library.Aplication.UseCases.UserCases.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.UserCases.Commands
+{
+    public class CreateUserCommand:UserDto,IRequest<bool>
+    {
+    }
+}
