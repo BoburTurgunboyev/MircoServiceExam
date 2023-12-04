5e65a79859b33dc83448de149cb6f8231d5d89d7
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 01:49:39 +0500

    Getall yozildi

---
+++ Library.Aplication/UseCases/User_BookCase/Dtos/User_BookDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.User_BookCase.Dtos
+{
+    public class User_BookDto
+    {
+        public bool StatusActive { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+    }
+}
