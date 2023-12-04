793813a9191cc3a86aa25c363582396d94d6ecae
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:14:26 +0500

    databasani uladim

---
+++ src/Library/Library.Domain/Entities/User_Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class User_Book
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(Book))]
+        public int BookId {  get; set; }
+        public Book Book { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public bool StatusActive { get; set; }
+    }
+}
