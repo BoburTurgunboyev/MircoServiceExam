bca4bfbc8146854d7b790f051de63f4708452069
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:14:20 +0500

    databasani uladim

---
+++ src/Library/Library.Domain/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Enums
+{
+    public enum Role
+    {
+        User =1,
+        Admin,
+        SuperAdmin
+    }
+}
