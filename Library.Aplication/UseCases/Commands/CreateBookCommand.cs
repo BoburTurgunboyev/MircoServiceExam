b6a21a76a2dac87e235a79a56da9aee5d79ad92e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 22:14:28 +0500

    BookApi

---
+++ Library.Aplication/UseCases/Commands/CreateBookCommand.cs
@@ -0,0 +1,15 @@
+﻿using Library.Aplication.UseCases.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Library.Aplication.UseCases.Commands
+{
+    public class CreateBookCommand: BookDto,IRequest<bool>
+    {
+    }
+}
