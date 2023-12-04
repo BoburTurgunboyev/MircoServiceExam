b6cea91e4f41c08a2e3d791be0eb6e7d23eb9447
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:33 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/UserCase/Dtos/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.UserCases.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+        public string UserAdress { get; set; }
+        public string Email { get; set; }
+        public string UserPhone { get; set; }
+        public Role UserRole { get; set; }
+    }
+}
