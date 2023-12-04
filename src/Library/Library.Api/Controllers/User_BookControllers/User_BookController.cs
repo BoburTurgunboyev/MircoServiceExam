e869953b81a79b9a00faa3f22e38f8ac434d4bb5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 01:49:16 +0500

    Getall yozildi

---
+++ src/Library/Library.Api/Controllers/User_BookControllers/User_BookController.cs
@@ -0,0 +1,34 @@
+﻿using Library.Aplication.UseCases.User_BookCase.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers.User_BookControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class User_BookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public User_BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateUserBookAsync(CreateUser_BookCommand command)
+        {
+            var Userbook = new CreateUser_BookCommand()
+            {
+                BookId = command.BookId,
+                UserId = command.UserId,
+                StatusActive = command.StatusActive,
+            };
+            await _mediator.Send(Userbook);
+            return Ok(command);
+        } 
+
+    }
+}
