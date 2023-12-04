66a253f97dc3b3b475e0101e299eee81adab31cd
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:15 +0500

    libarayApi

---
+++ src/Library/Library.Api/Controllers/BookControllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿
+using Library.Aplication.UseCases.UserCases.Commands;
+using Library.Aplication.UseCases.UserCases.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers.UserControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        { 
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateBook(BookDto bookDto)
+        {
+            var res = new CreateBookCommand()
+            {
+                DatePublished = bookDto.DatePublished,
+                BookCategoryId = bookDto.BookCategoryId,
+                Title = bookDto.Title,
+                PublishPlace = bookDto.PublishPlace,
+
+            };
+            await _mediator.Send(res);
+            return Ok(bookDto);
+        }
+    }
+}
