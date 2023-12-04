2483dd2ec428d9730bbd28a4e32bc2add5d2958a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:20 +0500

    libarayApi

---
+++ src/Library/Library.Api/Controllers/BookCategoryControllers/BookCategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Library.Aplication.UseCases.BookCategoryCase.Commands;
+using Library.Aplication.UseCases.BookCategoryCase.Dtos;
+using Library.Aplication.UseCases.UserCases.Commands;
+using Library.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers.BookCategoryControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookCategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookCategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateBookCategory(BookCategoryDto bookCategoryDto)
+        {
+            var bookcategory = new CreateBookCategoryCommand()
+            {
+                CategoryName = bookCategoryDto.CategoryName,
+                Block = bookCategoryDto.Block,
+                LibraryId = bookCategoryDto.LibraryId,
+            };
+
+            await _mediator.Send(bookcategory);
+            return Ok(bookcategory);
+        }
+    }
+}
