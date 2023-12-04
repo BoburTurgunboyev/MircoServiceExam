a961c7f69a81c8d3ffc1a50f1f7ca19e9c1d337b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:15:20 +0500

    databasani uladim

---
+++ Library.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
