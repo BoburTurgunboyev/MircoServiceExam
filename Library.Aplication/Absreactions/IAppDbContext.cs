4b0e4d64089b273f2d6b7cf0bd93d452a6a168a6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:15:47 +0500

    databasani uladim

---
+++ Library.Aplication/Absreactions/IAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Library.Aplication.Absreactions
+{
+    public  interface IAppDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<User_Book> Users_Books { get; set; }
+        public DbSet<Domain.Entities.Library> Libraries { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
