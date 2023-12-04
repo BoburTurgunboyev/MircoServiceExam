63e9e2b7011d02b23cf479abd189376eeb5eea99
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:14:31 +0500

    databasani uladim

---
+++ src/Library/Library.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Enums;
+
+namespace Library.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+        public string UserAdress { get; set; }
+        public string Email { get; set; }
+        public string UserPhone { get; set; }
+        public Role UserRole { get; set; }
+
+        public ICollection<User_Book> User_Books { get; set; }
+
+
+    }
+}
