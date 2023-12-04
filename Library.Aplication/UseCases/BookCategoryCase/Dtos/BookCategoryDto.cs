13427252adac04f6d94eaee5cce90173be0a2871
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:56:13 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/BookCategoryCase/Dtos/BookCategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCategoryCase.Dtos
+{
+    public class BookCategoryDto
+    {
+        public string CategoryName { get; set; }
+        public string Block { get; set; }
+        public int? LibraryId { get; set; }
+
+    }
+}
