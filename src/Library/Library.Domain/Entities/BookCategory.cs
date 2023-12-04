8ac3d492ed00d80ee92b22de968b0af41bf6c4d2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:14:40 +0500

    databasani uladim

---
+++ src/Library/Library.Domain/Entities/BookCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain.Entities
+{
+    public class BookCategory
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Block { get; set; }
+
+
+        [ForeignKey(nameof(Library))]
+        public int? LibraryId { get; set; }
+        public Library? Library { get; set; }
+
+        public ICollection<Book> Books { get; set;}
+      
+    }
+}
+
