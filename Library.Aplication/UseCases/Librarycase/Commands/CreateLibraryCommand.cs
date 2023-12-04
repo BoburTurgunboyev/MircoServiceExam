bc9eb3173896c026f86c2de6475dc7c1aaa15e65
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:51 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/Librarycase/Commands/CreateLibraryCommand.cs
@@ -0,0 +1,14 @@
+﻿using Library.Aplication.UseCases.Librarycase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Commands
+{
+    public class CreateLibraryCommand :LibraryDto, IRequest<bool>
+    {
+    }
+}
