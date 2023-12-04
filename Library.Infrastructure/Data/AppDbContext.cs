ff00e39ee957b4afb3a60d797f2d9e324c6218a6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:15:35 +0500

    databasani uladim

---
+++ Library.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Data
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<User_Book> Users_Books { get; set; }
+        public DbSet<Domain.Entities.Library> Libraries { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+
+
+
+    }
+}
