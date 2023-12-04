b53a4a8f832f2b6bcc88072dd319654f02112d4c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:55:47 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/Librarycase/Dtos/LibraryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Dtos
+{
+    public class LibraryDto
+    {
+        public string LibraryName { get; set; }
+        public string LibraryNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
