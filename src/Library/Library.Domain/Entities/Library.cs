0a62a257431b4f21318d4d3af4fe8803bf43111d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-04 21:14:35 +0500

    databasani uladim

---
+++ src/Library/Library.Domain/Entities/Library.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Domain.Entities
+{
+    public class Library
+    {
+        public int Id { get; set; }
+        public string LibraryName { get; set; }
+        public string LibraryNumber { get; set; }
+        public string Location { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+
+    }
+}
