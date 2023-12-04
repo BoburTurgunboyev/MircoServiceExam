137765f78839d8a1eda28d1d7a43abf16fb99195
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 00:56:18 +0500

    libarayApi

---
+++ Library.Aplication/UseCases/BookCategoryCase/Commands/CreateBookCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using Library.Aplication.UseCases.BookCategoryCase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCategoryCase.Commands
+{
+    public class CreateBookCategoryCommand : BookCategoryDto,IRequest<bool>
+    {
+    }
+}
