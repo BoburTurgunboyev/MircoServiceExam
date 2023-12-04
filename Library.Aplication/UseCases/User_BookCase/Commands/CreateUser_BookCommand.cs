a0be62480a7da3a9e594cd1981b062ceee1cd635
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 01:49:43 +0500

    Getall yozildi

---
+++ Library.Aplication/UseCases/User_BookCase/Commands/CreateUser_BookCommand.cs
@@ -0,0 +1,14 @@
+﻿using Library.Aplication.UseCases.User_BookCase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.User_BookCase.Commands
+{
+    public class CreateUser_BookCommand:User_BookDto,IRequest<bool>
+    {
+    }
+}
