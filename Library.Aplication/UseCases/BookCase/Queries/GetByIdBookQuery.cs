3caf204b49b6115e0ef594d5c8006b842d694b39
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:48 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCase/Queries/GetByIdBookQuery.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCase.Queries
+{
+    public class GetByIdBookQuery : IRequest<Book>
+    {
+        public int Id { get; set; } 
+    }
+}
