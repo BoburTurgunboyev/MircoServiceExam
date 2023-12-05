d833d3e230996d77eef1d6a221ae8b54a15e9ba1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:23 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Queries/GetByIdLibraryQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Queries
+{
+    public class GetByIdLibraryQuery : IRequest<Domain.Entities.Library>
+    {
+        public int Id { get; set; } 
+    }
+}
