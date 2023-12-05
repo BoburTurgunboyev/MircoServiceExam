1c0e3b8620b7b017c174a1bd223f55dcf65003fd
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:59 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Domain/Entities/Odam.cs
@@ -0,0 +1,27 @@
+﻿using Mahalla.Domain.Enumes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Domain.Entities
+{
+    public  class Odam
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int HomeNumber {  get; set; }
+        public int Age {  get; set; }
+        public string Number {  get; set; }
+        public Male OdamMale { get; set; }
+
+
+        [ForeignKey(nameof(Oila))]
+        public int OilaId { get; set; }
+        public Oila Oila { get; set; }  
+    }
+}
