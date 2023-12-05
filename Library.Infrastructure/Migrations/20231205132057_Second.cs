0c6da5d48ea363fc7bd5a41e4a8a6a45968ebc4a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 20:07:43 +0500

    Configration qoshdildi

---
+++ Library.Infrastructure/Migrations/20231205132057_Second.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Users");
+        }
+    }
+}
