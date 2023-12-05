7cd8f50841168f3ead645202677bff808455cef1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:50 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Domain/Enumes/Male.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Domain.Enumes
+{
+    public enum Male
+    {
+        ayol =1,
+        erkak
+    }
+}
