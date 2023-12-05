d86ff8d536f94938c96da0d3ce595a7f7448ae99
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:33 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Handlers/UpdateLibraryCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.Librarycase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Handlers
+{
+    public class UpdateLibraryCommandHandler : IRequestHandler<UpdateLibraryCommand, bool>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public UpdateLibraryCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(UpdateLibraryCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.Libraries.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (res == null) 
+            {
+                return false;
+            }
+            res.Id = request.Id;
+            res.LibraryNumber = request.LibraryNumber;
+            res.LibraryName = request.LibraryName;
+            res.Location = request.Location;
+
+             _appDbContext.Libraries.Update(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
