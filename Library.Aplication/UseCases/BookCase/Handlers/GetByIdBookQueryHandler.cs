ea6d0843a1555870548fac7aa96972dd51c270f6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:56 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCase/Handlers/GetByIdBookQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.BookCase.Queries;
+using Library.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCase.Handlers
+{
+    public class GetByIdBookQueryHandler : IRequestHandler<GetByIdBookQuery, Book>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetByIdBookQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Book> Handle(GetByIdBookQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbContext.Books.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
