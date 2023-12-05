757b3e42744f1bbcf99645eef771ec5e511a7970
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:40 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Handlers/GetByIdLibraryQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.Librarycase.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Handlers
+{
+    public class GetByIdLibraryQueryHandler : IRequestHandler<GetByIdLibraryQuery, Domain.Entities.Library>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetByIdLibraryQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Domain.Entities.Library> Handle(GetByIdLibraryQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.Libraries.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return res;
+        }
+    }
+}
