3855afb34ec9e365f3e765fd21a3e7d6918cfd46
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:40 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Infrastructure/Data/AppDbConect.cs
@@ -0,0 +1,22 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Infrastructure.Data
+{
+    public class AppBbContect : DbContext, IAppDbConect
+    {
+
+        public AppBbContect(DbContextOptions<AppBbContect> options):base (options) { }   
+
+        public DbSet<MahallaKamitet> MahallaKamitets { get; set; }
+        public DbSet<Rais> Raiss {  get; set; }
+        public DbSet<Oila> Oilas {  get; set; }
+        public DbSet<Odam> Odams {  get; set; }
+    }
+}
