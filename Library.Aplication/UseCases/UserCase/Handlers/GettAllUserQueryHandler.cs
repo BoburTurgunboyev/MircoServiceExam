a54b094dd1ca54dfa7cb221e01e4e9f8b8b1d7c1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:34:59 +0500

    crud

---
+++ Library.Aplication/UseCases/UserCase/Handlers/GettAllUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.UserCase.Queries;
+using Library.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.UserCase.Handlers
+{
+    public class GettAllUserQueryHandler : IRequestHandler<GetAllUserQuery, List<User>>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GettAllUserQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<List<User>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.Users.ToListAsync(cancellationToken);
+            return res;
+        }
+    }
+}
