08d8d15d4e4f378116c787e1fa865b3632868aec
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:28 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Queries/GetAllLibraryQuery.cs
@@ -0,0 +1,14 @@
+﻿using Library.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Queries
+{
+    public class GetAllLibraryQuery  : IRequest<List<Domain.Entities.Library>>
+    {
+    }
+}
