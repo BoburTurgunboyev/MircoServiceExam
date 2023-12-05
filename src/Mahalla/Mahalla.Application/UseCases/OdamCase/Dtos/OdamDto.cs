dd2813f303622160067b85acb42fa0fa741c85b6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 16:52:24 +0500

    case va controller yaratildi

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Dtos/OdamDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OdamCase.Dtos
+{
+    internal class OdamDto
+    {
+    }
+}
