3870d0b123b2d612bdcd6f879ece341bd9ae498d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:00 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Commands/DeleteLibraryCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Commands
+{
+    public class DeleteLibraryCommand :IRequest<bool>
+    {
+        public int Id { get; set; } 
+    }
+}
