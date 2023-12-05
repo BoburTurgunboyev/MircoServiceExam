79c21b4e12f6eb5d2fa0aa334894880776f7436f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:55 +0500

    crud

---
+++ Library.Aplication/UseCases/Librarycase/Commands/UpdateLibraryCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.Librarycase.Commands
+{
+    public class UpdateLibraryCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string LibraryName { get; set; }
+        public string LibraryNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
