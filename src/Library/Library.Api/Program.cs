f3e3c9189cddd5b16cdaad6f79fc615c64f8a5e5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 20:07:17 +0500

    Configration qoshdildi

--- src/Library/Library.Api/Program.cs
+++ src/Library/Library.Api/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAplication();
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
