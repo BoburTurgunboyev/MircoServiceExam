44a52774a1f9cca47557f388248cbc6c4e3841c9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:34:33 +0500

    crud

--- src/Library/Library.Api/Controllers/BookCategoryControllers/BookCategoryController.cs
+++ src/Library/Library.Api/Controllers/BookCategoryControllers/BookCategoryController.cs
@@ -1,6 +1,11 @@
-﻿using Library.Aplication.UseCases.BookCategoryCase.Commands;
+﻿using Library.Aplication.UseCases.BookCase.Commands;
+using Library.Aplication.UseCases.BookCategoryCase.Commands;
 using Library.Aplication.UseCases.BookCategoryCase.Dtos;
+using Library.Aplication.UseCases.BookCategoryCase.Queries;
+using Library.Aplication.UseCases.UserCase.Commands;
+using Library.Aplication.UseCases.UserCase.Queries;
 using Library.Aplication.UseCases.UserCases.Commands;
+using Library.Aplication.UseCases.UserCases.Dtos;
 using Library.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -32,6 +37,42 @@ namespace Library.Api.Controllers.BookCategoryControllers
 
             await _mediator.Send(bookcategory);
             return Ok(bookcategory);
+        }
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetAllBookCategory()
+        {
+            var res = await _mediator.Send(new GetAllBookCategoryQuery());
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdBookCategory(int id)
+        {
+            var res = await _mediator.Send(new GetByIdBookCategoryQuery() {Id=id});
+            return Ok(res);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateBookCategory(int id, BookCategoryDto bookCategoryDto)
+        {
+            var res = new UpdateBookCategoryCommand()
+            {
+                Id = id,
+                CategoryName = bookCategoryDto.CategoryName,
+                Block = bookCategoryDto.Block,
+                LibraryId = bookCategoryDto.LibraryId,
+             
+
+            };
+            await _mediator.Send(res);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteBookCategory(int id)
+        {
+            var res = await _mediator.Send(new DeleteBookCategoryCommand() { Id = id });
+            return Ok("Deleted");
         }
     }
 }
