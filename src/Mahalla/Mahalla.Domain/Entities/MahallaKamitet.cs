c0365aa788590ba62254c3831cc957f89f050b80
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:37:03 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Domain/Entities/MahallaKamitet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Domain.Entities
+{
+    public class MahallaKamitet
+    {
+        public int Id { get; set; } 
+        public string MahallaName { get; set; }
+        public string CityName { get; set; }
+        public string RegionName { get; set; }
+
+        public int RaisId { get; set; }
+        public Rais Rais { get; set; }
+
+        public ICollection<Oila> Oilas { get; set; }
+        
+    }
+}
