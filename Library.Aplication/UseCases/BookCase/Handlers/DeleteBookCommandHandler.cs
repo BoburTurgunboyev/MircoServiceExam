500ed174931422f981ccbf42c019251cd9a29de2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:37:00 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCase/Handlers/DeleteBookCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.BookCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCase.Handlers
+{
+    public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, bool>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public DeleteBookCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<bool> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.Books.FirstOrDefaultAsync(x =>x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+
+             _appDbContext.Books.Remove(res);
+            var result = await _appDbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
