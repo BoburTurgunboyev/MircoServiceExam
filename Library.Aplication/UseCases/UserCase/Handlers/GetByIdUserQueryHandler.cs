befaa31144563d055324b468a911114e81eb8034
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:04 +0500

    crud

---
+++ Library.Aplication/UseCases/UserCase/Handlers/GetByIdUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Library.Aplication.Absreactions;
+using Library.Aplication.UseCases.UserCase.Queries;
+using Library.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.UserCase.Handlers
+{
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, User>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public GetByIdUserQueryHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<User> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return res;
+        }
+    }
+}
