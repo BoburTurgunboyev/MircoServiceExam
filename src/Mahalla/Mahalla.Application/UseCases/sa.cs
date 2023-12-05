6276333311f780d5f1612c9a05a0771a76303f80
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:37:07 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Application/UseCases/sa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases
+{
+    internal class sa
+    {
+    }
+}
