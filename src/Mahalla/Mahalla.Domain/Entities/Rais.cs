49666b3015bb0a0579d4323f3f65c9b00b5669d6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:03 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Domain/Entities/Rais.cs
@@ -0,0 +1,23 @@
+﻿using Mahalla.Domain.Enumes;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Mahalla.Domain.Entities
+{
+    public class Rais
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }    
+        public string Number {  get; set; }
+        public string Email { get; set; }
+        public Male RaisMale { get; set; }
+
+
+        [ForeignKey(nameof(MahallaKamitet))]
+        public int MahallaKamitetId { get; set; }
+        public MahallaKamitet MahallaKamitet { get; set; }
+
+    }
+}
