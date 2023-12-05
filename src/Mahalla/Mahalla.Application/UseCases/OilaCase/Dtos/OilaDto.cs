22ae351d62fa0a27cf4df460e62d0a610ca332a6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 16:52:03 +0500

    case va controller yaratildi

---
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Dtos/OilaDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OilaCase.Dtos
+{
+    internal class OilaDto
+    {
+    }
+}
