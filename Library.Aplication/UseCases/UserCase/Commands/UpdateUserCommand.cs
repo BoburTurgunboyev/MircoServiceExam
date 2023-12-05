2a151c92049d18abc8998ec569d2690bbb1c5d7d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 20:08:46 +0500

    Configration qoshdildi

--- Library.Aplication/UseCases/UserCase/Commands/UpdateUserCommand.cs
+++ Library.Aplication/UseCases/UserCase/Commands/UpdateUserCommand.cs
@@ -1,5 +1,7 @@
-﻿using Library.Domain.Enums;
+﻿using Library.Aplication.UseCases.UserCases.Dtos;
+using Library.Domain.Enums;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +10,8 @@ using System.Threading.Tasks;
 
 namespace Library.Aplication.UseCases.UserCase.Commands
 {
-    public class UpdateUserCommand:IRequest<bool>
+    public class UpdateUserCommand:UserDto,IRequest<bool>
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string? UserName { get; set; }
-        public int UserAge { get; set; }
-        public string UserAdress { get; set; }
-        public string Email { get; set; }
-        public string? ImageUrl { get; set; }
-        public string UserPhone { get; set; }
-        public Role UserRole { get; set; }
     }
 }
