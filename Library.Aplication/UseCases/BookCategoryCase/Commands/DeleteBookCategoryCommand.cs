0e5d465b78f981f0db8364eda2880a3b257952a6
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:43 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCategoryCase/Commands/DeleteBookCategoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCategoryCase.Commands
+{
+    public class DeleteBookCategoryCommand : IRequest<bool>
+    {
+        public int Id { get; set; } 
+    }
+}
+ 
