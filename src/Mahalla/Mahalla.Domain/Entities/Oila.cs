ab30401e13a84cd6842f80f40fd2ffa64fe4ef56
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:54 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Domain/Entities/Oila.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Domain.Entities
+{
+    public class Oila
+    {
+        public int Id { get; set; }
+        public int member {  get; set; }
+
+
+        [ForeignKey(nameof(MahallaKamitet))]
+        public int MahallaKamitetId { get; set; }
+        public MahallaKamitet MahallaKamitet { get; set; }
+
+        public ICollection<Odam> Odams { get; set; }
+    }
+}
