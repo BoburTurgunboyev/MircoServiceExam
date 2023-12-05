6c31d0f0ff2f8d1644761f52d2629365f299f156
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:35:48 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿
+using Mahalla.Application.Absreactions;
+using Mahalla.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IAppDbConect, AppBbContect>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
