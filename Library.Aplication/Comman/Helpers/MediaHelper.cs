47b8460f19def6f77b6a003fca81dc875bf0f077
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 20:09:03 +0500

    Configration qoshdildi

---
+++ Library.Aplication/Comman/Helpers/MediaHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.Comman.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetImageExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "IMG_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            return null;
+        }
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+            ".jpg", ".jpeg",
+            ".png",
+            ".bmp",
+            ".svg"
+            };
+        }
+    }
+}
