3a235e63e9c2d2a19336c7bdc302c4e98963646b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 16:53:07 +0500

    case va controller yaratildi

---
+++ src/Mahalla/Mahalla.Api/Controllers/MahallaKamitetControllers/MahallaKamitetController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mahalla.Api.Controllers.MahallaKamitetControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahallaKamitetController : ControllerBase
+    {
+    }
+}
