197cd463abca230b7fba854838ea8bd116faa848
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:39 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCategoryCase/Commands/UpdateBookCategoryCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCategoryCase.Commands
+{
+    public class UpdateBookCategoryCommand : IRequest<bool>
+    {
+        public int Id { get; set; } 
+        public string CategoryName { get; set; }
+        public string Block { get; set; }
+        public int? LibraryId { get; set; }
+
+    }
+}
