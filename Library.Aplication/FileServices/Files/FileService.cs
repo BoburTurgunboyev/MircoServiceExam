74d197861fe0608f83d4e5de6cf727c17fb8fea3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 20:08:54 +0500

    Configration qoshdildi

---
+++ Library.Aplication/FileServices/Files/FileService.cs
@@ -0,0 +1,36 @@
+﻿using Library.Aplication.Comman.Helpers;
+using Library.Aplication.Interface.File;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.FileServices.Files
+{
+    public class FileService:IFileService
+    {
+        private readonly string MEDIA = "media";
+        private readonly string IMAGES = "images";
+        private readonly string ROOTPATH;
+        public FileService()
+        {
+            ROOTPATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        }
+
+    
+        public async ValueTask<string> UploadImageAsync(IFormFile file)
+        {
+            string newImageName = MediaHelper.MakeImageName(file.FileName.ToLower());
+            string subPath = Path.Combine(MEDIA, IMAGES, newImageName);
+            string path = Path.Combine(ROOTPATH, subPath);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                return subPath;
+            }
+        }
+    }
+}
