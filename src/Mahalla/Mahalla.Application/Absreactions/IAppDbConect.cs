ad61d6740e68ba1fe0ebd4d85582e94c96ef1f9a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 15:36:19 +0500

    2-proyekt yaratildi

---
+++ src/Mahalla/Mahalla.Application/Absreactions/IAppDbConect.cs
@@ -0,0 +1,20 @@
+﻿using Mahalla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.Absreactions
+{
+    public interface IAppDbConect
+    {
+        public DbSet<MahallaKamitet> MahallaKamitets { get; set; }
+        public DbSet<Rais> Raiss { get; set; }
+        public DbSet<Oila> Oilas { get; set; }  
+        public DbSet <Odam> Odams { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
