a72f552afbb022ecd665d4dfd6f1e001c5091810
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:35:13 +0500

    crud

---
+++ Library.Aplication/UseCases/UserCase/Commands/UpdateUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using Library.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.UserCase.Commands
+{
+    public class UpdateUserCommand:IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? UserName { get; set; }
+        public int UserAge { get; set; }
+        public string UserAdress { get; set; }
+        public string Email { get; set; }
+        public string UserPhone { get; set; }
+        public Role UserRole { get; set; }
+    }
+}
