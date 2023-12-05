1efa584a569c1451d16d5011332d5c75d5b4150f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-05 05:36:05 +0500

    crud

---
+++ Library.Aplication/UseCases/BookCategoryCase/Queries/GetByIdBookCategoryQuery.cs
@@ -0,0 +1,15 @@
+﻿using Library.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Aplication.UseCases.BookCategoryCase.Queries
+{
+    public class GetByIdBookCategoryQuery:IRequest<BookCategory>
+    {
+        public int Id { get; set; } 
+    }
+}
