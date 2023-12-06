d919c35c842098e4aceb0d75080857867096aceb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:26 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Queries/GetByIdOdamQuery.cs
@@ -0,0 +1,14 @@
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Mahalla.Application.UseCases.OdamCase.Queries
+{
+    public class GetByIdOdamQuery : IRequest<Odam>
+    {
+        public int Id { get; set; }
+    }
+}
