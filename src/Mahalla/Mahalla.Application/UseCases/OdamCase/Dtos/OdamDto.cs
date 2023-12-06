8cf3f94177a9b4772e2b4b1544408c952ed01304
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:02 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/OdamCase/Dtos/OdamDto.cs
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Dtos/OdamDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mahalla.Domain.Enumes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.OdamCase.Dtos
 {
-    internal class OdamDto
+    public class OdamDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int HomeNumber { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+        public Male OdamMale { get; set; }
+        public int OilaId { get; set; }
     }
 }
