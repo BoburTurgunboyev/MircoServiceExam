d3cab62969a742bcdd5af8d658d246f7f0084c6a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:24 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Queries/GetByIdMahallaKomitetQuery.cs
@@ -0,0 +1,15 @@
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.MahallaKamitetCase.Queries
+{
+    public class GetByIdMahallaKomitetQuery:IRequest<MahallaKamitet>
+    {
+        public int Id { get; set; } 
+    }
+}
