c6a741bf054b250f1473dedd41615e704eed5919
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:41:12 +0500

    3-project boshlandi

---
+++ University.Infrastructure/Data/AppDbConnection.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Domain.Entities;
+
+namespace University.Infrastructure.Data
+{
+    public class AppDbConnection : DbContext, IAppDbConection
+    {
+        public AppDbConnection(DbContextOptions<AppDbConnection> options):base(options) { }
+
+        public DbSet<Students> studentss { get; set; }
+        public DbSet<StudentSubjects> studentSubjectss { get; set; }
+        public DbSet<OverAllMarks> overAllMarkss { get; set; }   
+        public DbSet<ExamResults> examResultss { get; set; } 
+        public DbSet<Subjects> subjectss { get; set; }
+    }
+}
