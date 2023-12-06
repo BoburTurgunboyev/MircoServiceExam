2726ce0af52ce4bd266afb0ec14e7b7455b95e6b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:46:50 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/OilaCase/Handlers/CreateOilaCommandHandler.cs
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Handlers/CreateOilaCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.OilaCase.Commands;
+using Mahalla.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,28 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.OilaCase.Handlers
 {
-    internal class CreateOilaCommandHandler
+    public class CreateOilaCommandHandler : IRequestHandler<CreateOilaCommand, bool>
     {
+        private readonly IAppDbConect _appDbConect;
+
+        public CreateOilaCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(CreateOilaCommand request, CancellationToken cancellationToken)
+        {
+            var oila = new Oila()
+            {
+                Id = request.Id,
+                member = request.member,
+                MahallaKamitetId =request.MahallaKamitetId,
+
+            };
+
+            _appDbConect.Oilas.Add(oila);
+            var res= await _appDbConect.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
     }
 }
