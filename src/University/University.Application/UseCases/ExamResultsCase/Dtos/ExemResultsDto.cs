8a33ed1b5dab70b9574918f4aee8b468eef5a85e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:43:32 +0500

    3-project boshlandi

---
+++ src/University/University.Application/UseCases/ExamResultsCase/Dtos/ExemResultsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Enums;
+
+namespace University.Application.UseCases.ExamResultsCase.Dtos
+{
+    public class ExemResultsDto
+    {
+        
+        public Term Terms { get; set; }
+        public int ExemMark { get; set; }
+        public int Year { get; set; }
+        public int StudentSubjectId { get; set; }
+    }
+}
