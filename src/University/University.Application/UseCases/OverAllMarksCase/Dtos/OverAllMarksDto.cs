c00e66b9b3863aee4279d34d51f79f6e7284e28b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:43:12 +0500

    3-project boshlandi

---
+++ src/University/University.Application/UseCases/OverAllMarksCase/Dtos/OverAllMarksDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Enums;
+
+namespace University.Application.UseCases.OverAllMarksCase.Dtos
+{
+    public class OverAllMarksDto
+    {
+        public int Id { get; set; }
+        public MarkType MarkType { get; set; }
+
+        public int StudentsId { get; set; }
+    }
+}
