7386f87daf5c0d72ae60a76dcd24a4eb090d0835
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:44:51 +0500

    3-project boshlandi

---
+++ src/Mahalla/Mahalla.Infrastructure/Configration/Oillas/OilaConfigration.cs
@@ -0,0 +1,25 @@
+﻿using Mahalla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Infrastructure.Configration.Oillas
+{
+    public class OilaConfigration : IEntityTypeConfiguration<Oila>
+    {
+        public void Configure(EntityTypeBuilder<Oila> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.member).IsRequired();
+
+            entity.HasOne(e => e.MahallaKamitet)
+                .WithMany(m => m.Oilas)
+                .HasForeignKey(e => e.MahallaKamitetId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
