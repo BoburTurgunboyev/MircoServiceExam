67c5450289e790dcfd6950d9c7449fa847dd8130
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:52 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Handlers/DeleteOdamCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.OdamCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OdamCase.Handlers
+{
+    public class DeleteOdamCommandHandler : IRequestHandler<DeleteOdamCommand, bool>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public DeleteOdamCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(DeleteOdamCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.Odams.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (res == null)
+            {
+                return false;
+            }
+
+            _appDbConect.Odams.Remove(res);
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
