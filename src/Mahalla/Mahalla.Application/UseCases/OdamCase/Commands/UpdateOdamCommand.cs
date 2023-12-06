a2d473c96f0bccd7c1173ed764a81ec1a1637677
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:08 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Commands/UpdateOdamCommand.cs
@@ -0,0 +1,23 @@
+﻿using Mahalla.Domain.Enumes;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OdamCase.Commands
+{
+    public class UpdateOdamCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int HomeNumber { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+        public Male OdamMale { get; set; }
+        public int OilaId { get; set; }
+    }
+}
