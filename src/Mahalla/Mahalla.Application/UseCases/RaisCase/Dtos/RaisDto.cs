c296c87eeb67dfbb1621adca7ed174f59e510275
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:54 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/RaisCase/Dtos/RaisDto.cs
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Dtos/RaisDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mahalla.Domain.Enumes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.RaisCase.Dtos
 {
-    internal class RaisDto
+    public class RaisDto
     {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Number { get; set; }
+        public string Email { get; set; }
+        public Male RaisMale { get; set; }
+        public int MahallaKamitetId { get; set; }
     }
 }
