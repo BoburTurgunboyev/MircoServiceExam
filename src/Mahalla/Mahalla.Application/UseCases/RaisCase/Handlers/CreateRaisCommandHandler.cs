67a3c4fce2d19f8f9839b16cdc3829e4e56adfe9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:49 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/RaisCase/Handlers/CreateRaisCommandHandler.cs
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Handlers/CreateRaisCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.RaisCase.Commands;
+using Mahalla.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,32 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.RaisCase.Handlers
 {
-    internal class CreateRaisCommandHandler
+    public class CreateRaisCommandHandler : IRequestHandler<CreateRaisCommand, bool>
     {
+        private readonly IAppDbConect _appDbConect;
+
+        public CreateRaisCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(CreateRaisCommand request, CancellationToken cancellationToken)
+        {
+            var rais = new Rais()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Age = request.Age,
+                Number = request.Number,
+                Email = request.Email,
+                MahallaKamitetId=request.MahallaKamitetId,
+                RaisMale = request.RaisMale,
+
+            };
+
+            await _appDbConect.Raiss.AddAsync(rais);
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
