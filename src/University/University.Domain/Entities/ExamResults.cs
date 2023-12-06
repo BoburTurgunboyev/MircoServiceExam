73a17bee7eb4e28d62f45118e453d5c066369968
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:40:43 +0500

    3-project boshlandi

---
+++ src/University/University.Domain/Entities/ExamResults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Enums;
+
+namespace University.Domain.Entities
+{
+    public class ExamResults
+    {
+        public int Id { get; set; }
+        public Term Terms { get; set; }
+        public int ExemMark {  get; set; }
+        public int Year {  get; set; }
+        public int StudentSubjectId { get; set; }
+        public StudentSubjects StudentSubject { get; set; }
+    }
+}
