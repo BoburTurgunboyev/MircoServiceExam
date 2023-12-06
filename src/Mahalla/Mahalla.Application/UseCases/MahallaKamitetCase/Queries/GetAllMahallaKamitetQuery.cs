2bed8bbf40868c04003ff27d6de038fca61cdc54
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:29 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Queries/GetAllMahallaKamitetQuery.cs
@@ -0,0 +1,15 @@
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.MahallaKamitetCase.Queries
+{
+    public class GetAllMahallaKamitetQuery:IRequest<List<MahallaKamitet>>
+    {
+
+    }
+}
