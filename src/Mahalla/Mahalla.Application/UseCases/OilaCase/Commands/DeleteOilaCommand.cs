7cd2df4af087c48bf99578133ec4a1074e89a6d1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:15 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Commands/DeleteOilaCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OilaCase.Commands
+{
+    public class DeleteOilaCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
