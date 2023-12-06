09770d5c76f46d5cd7e1709dfb07d4fc25e34a7c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:43:41 +0500

    3-project boshlandi

---
+++ src/University/University.Application/Absreactions/IAppDbConection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Entities;
+
+namespace University.Application.Absreactions
+{
+    public interface IAppDbConection
+    {
+        public DbSet<Students> studentss { get; set; }
+        public DbSet<StudentSubjects> studentSubjectss { get; set; }
+        public DbSet<OverAllMarks> overAllMarkss { get; set; }
+        public DbSet<ExamResults> examResultss { get; set; }
+        public DbSet<Subjects> subjectss { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
