17eae32879786f59e60fd07159f9619686fc9efc
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:44:45 +0500

    3-project boshlandi

---
+++ src/Mahalla/Mahalla.Infrastructure/Configration/Raises/RaisConfigration.cs
@@ -0,0 +1,30 @@
+﻿using Mahalla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Infrastructure.Configration.Raises
+{
+    public class RaisConfigration : IEntityTypeConfiguration<Rais>
+    {
+        public void Configure(EntityTypeBuilder<Rais> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+            entity.Property(e => e.Age).IsRequired();
+            entity.Property(e => e.Number).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+
+
+
+            entity.HasOne(e => e.MahallaKamitet)
+                .WithOne(m => m.Rais)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
