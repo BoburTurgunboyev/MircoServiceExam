b28d3988e6cc6a8cb9d766473537e0d33860b68b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:39 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Handlers/GetAllRaisQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.RaisCase.Queries;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.RaisCase.Handlers
+{
+    public class GetAllRaisQueryHandler : IRequestHandler<GetAllRaisQuery, List<Rais>>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public GetAllRaisQueryHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<List<Rais>> Handle(GetAllRaisQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.Raiss.ToListAsync(cancellationToken);
+            return res;
+        }
+    }
+}
