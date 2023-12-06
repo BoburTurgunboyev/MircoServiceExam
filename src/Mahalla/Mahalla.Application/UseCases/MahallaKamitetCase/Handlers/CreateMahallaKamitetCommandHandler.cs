f6bec6311f66947752b9d35f1791cc005d0eb70c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:57 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Handlers/CreateMahallaKamitetCommandHandler.cs
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Handlers/CreateMahallaKamitetCommandHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.MahallaKamitetCase.Commands;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,31 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.MahallaKamitetCase.Handlers
 {
-    internal class CreateMahallaKamitetCommandHandler
+    public class CreateMahallaKamitetCommandHandler : IRequestHandler<CreateMahallaKamitetCommand, bool>
     {
+        private readonly IAppDbConect _appDbConect;
+
+        public CreateMahallaKamitetCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(CreateMahallaKamitetCommand request, CancellationToken cancellationToken)
+        {
+            var mahalla = new MahallaKamitet()
+            {
+              RegionName = request.RegionName,
+              MahallaName = request.MahallaName,
+              CityName = request.CityName,
+              RaisId= request.RaisId
+
+            };
+
+              await _appDbConect.MahallaKamitets.AddAsync(mahalla);
+
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result > 0;
+                  
+        }
     }
 }
