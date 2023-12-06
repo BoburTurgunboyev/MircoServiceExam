ca762a12c030784a530e613745348cfbcf56fbf2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:43 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Handlers/GetByIdOdamQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.OdamCase.Queries;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OdamCase.Handlers
+{
+    public class GetByIdOdamQueryHandler : IRequestHandler<GetByIdOdamQuery, Odam>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public GetByIdOdamQueryHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async  Task<Odam> Handle(GetByIdOdamQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbConect.Odams.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
