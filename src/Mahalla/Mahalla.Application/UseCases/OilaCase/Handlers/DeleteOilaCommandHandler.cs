de9c951e6ed9232ba5a2b1468c678ad55d2f96bb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:46:44 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Handlers/DeleteOilaCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.OilaCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OilaCase.Handlers
+{
+    public class DeleteOilaCommandHandler : IRequestHandler<DeleteOilaCommand, bool>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public DeleteOilaCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(DeleteOilaCommand request, CancellationToken cancellationToken)
+        {
+            var res =await _appDbConect.Oilas.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+            _appDbConect.Oilas.Remove(res);
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
