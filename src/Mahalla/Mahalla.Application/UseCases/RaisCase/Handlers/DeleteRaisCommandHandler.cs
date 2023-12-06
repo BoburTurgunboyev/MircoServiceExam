82ec9e283c07f4330b42661a5900c37a4e1233f1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:44 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Handlers/DeleteRaisCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.RaisCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.RaisCase.Handlers
+{
+    public class DeleteRaisCommandHandler : IRequestHandler<DeleteRaisCommand, bool>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public DeleteRaisCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(DeleteRaisCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.Raiss.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+            _appDbConect.Raiss.Remove(res);
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result>0;
+        }
+    }
+}
