072e7cf7116a5b1c6b8747bd97cd54c3c93e2d0f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:46:39 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Handlers/GetAllOilaQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.OilaCase.Queries;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OilaCase.Handlers
+{
+    public class GetAllOilaQueryHandler : IRequestHandler<GetAllOilaQuery, List<Oila>>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public GetAllOilaQueryHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<List<Oila>> Handle(GetAllOilaQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.Oilas.ToListAsync(cancellationToken);
+            return res;
+        }
+    }
+}
