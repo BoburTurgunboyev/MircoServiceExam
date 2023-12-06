658258ccf00fa646bb3f27746b60d9205ec4da54
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:46 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Handlers/GetAllMahallaKamitetQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.MahallaKamitetCase.Queries;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.MahallaKamitetCase.Handlers
+{
+    public class GetAllMahallaKamitetQueryHandler : IRequestHandler<GetAllMahallaKamitetQuery, List<MahallaKamitet>>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public GetAllMahallaKamitetQueryHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<List<MahallaKamitet>> Handle(GetAllMahallaKamitetQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.MahallaKamitets.ToListAsync(cancellationToken);
+            return res;
+        }
+    }
+}
