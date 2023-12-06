43f14413fc41c69ac0dcd2af81b52439b00e0072
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:58 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Commands/UpdateRaisCommand.cs
@@ -0,0 +1,17 @@
+﻿using Mahalla.Application.UseCases.OdamCase.Dtos;
+using Mahalla.Application.UseCases.RaisCase.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.RaisCase.Commands
+{
+    public class UpdateRaisCommand :RaisDto, IRequest<bool>
+    {
+        public int Id { get; set; }
+
+    }
+}
