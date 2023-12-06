e033a6e5bde5f170b665260c11d726f167c5d71d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:31 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/OdamCase/Queries/GetAllOdamQuery.cs
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Queries/GetAllOdamQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.OdamCase.Queries
 {
-    internal class GetAllOdamQuery
+    public class GetAllOdamQuery : IRequest<List<Odam>>
     {
     }
 }
