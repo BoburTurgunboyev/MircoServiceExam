1200755f18200948aecbd1842634eb6742a11a92
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:49:22 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Commands/CreateMahallaKamitetCommand.cs
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Commands/CreateMahallaKamitetCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mahalla.Application.UseCases.MahallaKamitetCase.Dtos;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.MahallaKamitetCase.Commands
 {
-    internal class CreateMahallaKamitetCommand
+    public class CreateMahallaKamitetCommand :MahallaKamitetDto, IRequest<bool>
     {
     }
 }
