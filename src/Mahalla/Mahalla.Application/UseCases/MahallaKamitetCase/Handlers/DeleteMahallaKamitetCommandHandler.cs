940325217d481765fc3cba55fc80cc23ed47fea9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:52 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Handlers/DeleteMahallaKamitetCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.MahallaKamitetCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.MahallaKamitetCase.Handlers
+{
+    public class DeleteMahallaKamitetCommandHandler : IRequestHandler<DeleteMahallaKamitetCommand,bool>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public DeleteMahallaKamitetCommandHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<bool> Handle(DeleteMahallaKamitetCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConect.MahallaKamitets.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (res == null) 
+            {
+                return false;
+            }
+
+            _appDbConect.MahallaKamitets.Remove(res);
+            var result = await _appDbConect.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
