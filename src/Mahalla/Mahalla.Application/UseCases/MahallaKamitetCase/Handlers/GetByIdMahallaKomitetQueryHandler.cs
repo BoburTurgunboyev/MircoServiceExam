6818b56000e7e4de21dbdaee1ca51e8e2a3401ec
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:41 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/MahallaKamitetCase/Handlers/GetByIdMahallaKomitetQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Mahalla.Application.Absreactions;
+using Mahalla.Application.UseCases.MahallaKamitetCase.Queries;
+using Mahalla.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.MahallaKamitetCase.Handlers
+{
+    public class GetByIdMahallaKomitetQueryHandler : IRequestHandler<GetByIdMahallaKomitetQuery, MahallaKamitet>
+    {
+        private readonly IAppDbConect _appDbConect;
+
+        public GetByIdMahallaKomitetQueryHandler(IAppDbConect appDbConect)
+        {
+            _appDbConect = appDbConect;
+        }
+
+        public async Task<MahallaKamitet> Handle(GetByIdMahallaKomitetQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbConect.MahallaKamitets.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
