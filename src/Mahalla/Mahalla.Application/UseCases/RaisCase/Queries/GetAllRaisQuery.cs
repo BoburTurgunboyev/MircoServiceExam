a821a8c4e4214137ef6f6f57343f152b2193a747
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:23 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/RaisCase/Queries/GetAllRaisQuery.cs
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Queries/GetAllRaisQuery.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.RaisCase.Queries
 {
-    internal class GetAllRaisQuery
+    public class GetAllRaisQuery : IRequest<List<Rais>>
     {
     }
 }
