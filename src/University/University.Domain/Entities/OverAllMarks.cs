7a4d46cc814e28488e41c92e3686a73ff108f6e9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:40:50 +0500

    3-project boshlandi

---
+++ src/University/University.Domain/Entities/OverAllMarks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Enums;
+
+namespace University.Domain.Entities
+{
+    public class OverAllMarks
+    {
+        public int Id {  get; set; }
+        public MarkType MarkType { get; set; }
+
+        public int StudentsId { get; set; }
+
+        public Students Students { get; set; }
+    }
+}
