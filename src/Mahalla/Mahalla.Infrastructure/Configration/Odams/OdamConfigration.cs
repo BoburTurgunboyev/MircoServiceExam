81721a4451e2e3f84b19854ab76f97317d788eb2
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:06 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Infrastructure/Configration/Odams/OdamConfigration.cs
@@ -0,0 +1,25 @@
+﻿using Mahalla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mahalla.Infrastructure.Configration.Odams
+{
+    public class OdamConfigration : IEntityTypeConfiguration<Odam>
+    {
+        public void Configure(EntityTypeBuilder<Odam> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.FirstName).IsRequired();
+            entity.Property(e => e.LastName).IsRequired();
+            entity.Property(e => e.StreetName).IsRequired();
+            entity.Property(e => e.HomeNumber).IsRequired();
+            entity.Property(e => e.Age).IsRequired();
+            entity.Property(e => e.Number).IsRequired();
+
+            entity.HasOne(e => e.Oila)
+            .WithMany(o => o.Odams)
+            .HasForeignKey(e => e.OilaId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
