9648ed0eb7405d390cc743af8092051d7c9ba83b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:41:28 +0500

    3-project boshlandi

---
+++ src/University/University.Domain/Enums/MarkType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Domain.Enums
+{
+    public enum MarkType
+    {
+        Alo = 1,
+        Yaxshi,
+        Qoniqarli,
+        Qoniqarsiz
+    }
+}
