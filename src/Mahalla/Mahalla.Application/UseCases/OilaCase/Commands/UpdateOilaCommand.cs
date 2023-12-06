3cd2c489ab137b815d04a9a9b5abe1b8f5aa6bf3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:01 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Commands/UpdateOilaCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OilaCase.Commands
+{
+    public class UpdateOilaCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public int member { get; set; }
+        public int MahallaKamitetId { get; set; }
+    }
+}
