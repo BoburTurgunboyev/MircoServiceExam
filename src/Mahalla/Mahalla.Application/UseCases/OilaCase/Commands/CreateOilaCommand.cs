9faadeb1b4be4ecfb0f0cd8b4148a13828259d0b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:47:20 +0500

    Crud

--- src/Mahalla/Mahalla.Application/UseCases/OilaCase/Commands/CreateOilaCommand.cs
+++ src/Mahalla/Mahalla.Application/UseCases/OilaCase/Commands/CreateOilaCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mahalla.Application.UseCases.OilaCase.Dtos;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Mahalla.Application.UseCases.OilaCase.Commands
 {
-    internal class CreateOilaCommand
+    public class CreateOilaCommand : OilaDto,IRequest<bool>
     {
+
     }
 }
