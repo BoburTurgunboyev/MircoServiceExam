380bb56019bad67d874652fce7f0c96cf9eff147
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:42:25 +0500

    3-project boshlandi

---
+++ src/University/University.Application/UseCases/StudentSubjectsCase/Dtos/StudentSubjectsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.UseCases.StudentSubjectsCases.Dtos
+{
+    public class StudentSubjectsDto
+    {
+        public int StudentId { get; set; }
+        public int SubjectsId { get; set; }
+    }
+}
