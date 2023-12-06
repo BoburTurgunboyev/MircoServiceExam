fe0575266ffafc52e74caa8f76054cfc618c2ce0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:48:13 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/OdamCase/Commands/DeleteOdamCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.OdamCase.Commands
+{
+    public class DeleteOdamCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
