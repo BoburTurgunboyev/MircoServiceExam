94ff0157d713e15dee3edc6d417cdb94bcd3992e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-06 21:45:18 +0500

    Crud

---
+++ src/Mahalla/Mahalla.Application/UseCases/RaisCase/Queries/GetByIdRaisQuery.cs
@@ -0,0 +1,15 @@
+﻿using Mahalla.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahalla.Application.UseCases.RaisCase.Queries
+{
+    public class GetByIdRaisQuery: IRequest<Rais>
+    {
+        public int Id { get; set; } 
+    }
+}
