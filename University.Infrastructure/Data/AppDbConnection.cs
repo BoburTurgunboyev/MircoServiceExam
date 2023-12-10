7ebe646bb83ee88b91fe7a7f6ce3a50ffd3acd8b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:34:31 +0500

    project 4 crud

--- University.Infrastructure/Data/AppDbConnection.cs
+++ University.Infrastructure/Data/AppDbConnection.cs
@@ -12,12 +12,20 @@ namespace University.Infrastructure.Data
 {
     public class AppDbConnection : DbContext, IAppDbConection
     {
-        public AppDbConnection(DbContextOptions<AppDbConnection> options):base(options) { }
+        public AppDbConnection(DbContextOptions<AppDbConnection> options):base(options) 
+        {
+            Database.Migrate();
+        }
 
         public DbSet<Students> studentss { get; set; }
-        public DbSet<StudentSubjects> studentSubjectss { get; set; }
+       
         public DbSet<OverAllMarks> overAllMarkss { get; set; }   
         public DbSet<ExamResults> examResultss { get; set; } 
         public DbSet<Subjects> subjectss { get; set; }
+
+        async ValueTask<int> IAppDbConection.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
