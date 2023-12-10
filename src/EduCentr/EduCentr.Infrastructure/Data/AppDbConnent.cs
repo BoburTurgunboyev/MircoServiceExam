b619d598b15735e2583215396f93aa414bddc1bb
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:42:19 +0500

    project 4 crud

---
+++ src/EduCentr/EduCentr.Infrastructure/Data/AppDbConnent.cs
@@ -0,0 +1,25 @@
+﻿using EduCentr.Application.Absreaction;
+using EduCentr.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduCentr.Infrastructure.Data
+{
+    public class AppDbConnent : DbContext, IAppDbConnect
+    {
+        public AppDbConnent(DbContextOptions<AppDbConnent> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<EduStudent> eduStudents { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+        async ValueTask<int> IAppDbConnect.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+            
+ 
+            
+        }
+    }
+}
