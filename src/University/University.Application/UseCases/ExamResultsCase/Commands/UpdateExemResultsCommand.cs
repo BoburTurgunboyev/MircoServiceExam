d5235cd2e5dbc8073206f886198a7005147ab9ab
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:47 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/ExamResultsCase/Commands/UpdateExemResultsCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.UseCases.ExamResultsCase.Dtos;
+
+namespace University.Application.UseCases.ExamResultsCase.Commands
+{
+    public class UpdateExemResultsCommand :ExemResultsDto, IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
