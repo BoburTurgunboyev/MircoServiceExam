a486d78f5fcf7351b4391e582d6ef011cd955e4c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:37:34 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/StudentsCase/Queries/GetByIdStudentsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Entities;
+
+namespace University.Application.UseCases.StudentsCase.Queries
+{
+    public class GetByIdStudentsQuery : IRequest<Students>
+    {
+        public int Id { get; set; }
+    }
+}
