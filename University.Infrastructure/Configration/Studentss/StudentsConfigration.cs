7fe2579a93c6b6ebe05901b629472cd2fe6f3a5c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:34:59 +0500

    project 4 crud

---
+++ University.Infrastructure/Configration/Studentss/StudentsConfigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Entities;
+
+namespace University.Infrastructure.Configration.Studentss
+{
+    public class StudentsConfigration : IEntityTypeConfiguration<Students>
+    {
+        public void Configure(EntityTypeBuilder<Students> builder)
+        {
+            
+        }
+    }
+}
