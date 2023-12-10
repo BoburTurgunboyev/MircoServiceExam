2e5955567d153046ec2764d280fdab82418d118e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:36:36 +0500

    project 4 crud

--- src/University/University.Application/UseCases/SubjectsCase/Handlers/CreateSubjectsCommandHandler.cs
+++ src/University/University.Application/UseCases/SubjectsCase/Handlers/CreateSubjectsCommandHandler.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.SubjectsCase.Commands;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.SubjectsCase.Handlers
 {
-    internal class CreateSubjectsCommandHandler
+    public class CreateSubjectsCommandHandler : IRequestHandler<CreateSubjectsCommand, bool>
     {
+        private readonly IAppDbConection _appDbConection;
+
+        public CreateSubjectsCommandHandler(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<bool> Handle(CreateSubjectsCommand request, CancellationToken cancellationToken)
+        {
+
+            var strudents = _appDbConection.studentss.ToList();
+            List<Students> edustudents = new List<Students>();
+
+            foreach (var s in strudents)
+            {
+                if (request.Subjects.Contains(s.Id))
+                {
+                    edustudents.Add(s); 
+                }
+            }
+            var subject = new Subjects()
+            {
+                SubjectName = request.SubjectName,
+                StudentSubject= edustudents
+                
+                
+            };
+            _appDbConection.subjectss.Add(subject);
+            var res = await _appDbConection.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
     }
 }
