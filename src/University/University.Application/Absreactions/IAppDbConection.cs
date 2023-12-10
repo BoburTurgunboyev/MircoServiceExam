6144deb26b6477b324dfbc7bc9d20f19dff2d271
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:40:00 +0500

    project 4 crud

--- src/University/University.Application/Absreactions/IAppDbConection.cs
+++ src/University/University.Application/Absreactions/IAppDbConection.cs
@@ -11,11 +11,11 @@ namespace University.Application.Absreactions
     public interface IAppDbConection
     {
         public DbSet<Students> studentss { get; set; }
-        public DbSet<StudentSubjects> studentSubjectss { get; set; }
+        
         public DbSet<OverAllMarks> overAllMarkss { get; set; }
         public DbSet<ExamResults> examResultss { get; set; }
         public DbSet<Subjects> subjectss { get; set; }
 
-        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        public ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
