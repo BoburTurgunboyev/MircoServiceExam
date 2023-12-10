736d48ce88a8af42423e213b55f7d282c32f2971
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:36:55 +0500

    project 4 crud

--- src/University/University.Application/UseCases/SubjectsCase/Commands/CreateSubjectsCommand.cs
+++ src/University/University.Application/UseCases/SubjectsCase/Commands/CreateSubjectsCommand.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.UseCases.StudentsCase.Dtos;
+using University.Application.UseCases.SubjectsCase.Dtos;
 
 namespace University.Application.UseCases.SubjectsCase.Commands
 {
-    internal class CreateSubjectsCommand
+    public class CreateSubjectsCommand :SubjectsDto, IRequest<bool>
     {
     }
 }
