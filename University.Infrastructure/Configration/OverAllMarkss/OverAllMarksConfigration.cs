e224c868653b6caa475d934faf34dab5f1097f3f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:35:35 +0500

    project 4 crud

---
+++ University.Infrastructure/Configration/OverAllMarkss/OverAllMarksConfigration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace University.Infrastructure.Configration.OverAllMarks
+{
+    public class OverAllMarksConfigration : IEntityTypeConfiguration<Domain.Entities. OverAllMarks>
+    {
+
+        public void Configure(EntityTypeBuilder<Domain.Entities.OverAllMarks> builder)
+        {
+
+        }
+    }
+}
