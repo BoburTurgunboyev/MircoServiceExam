a5541a7c33f39f38237b4387b6e5bb8d5ccc3285
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:38:18 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/StudentsCase/Commands/UpdateStudentsCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.UseCases.StudentsCase.Dtos;
+
+namespace University.Application.UseCases.StudentsCase.Commands
+{
+    public class UpdateStudentsCommand : StudentDto, IRequest<bool>
+    {
+        public int Id {  get; set; }
+    }
+}
