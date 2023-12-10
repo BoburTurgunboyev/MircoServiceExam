36d745a52a102b718fbcc717a7c4b33f0e96fbe3
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:36:40 +0500

    project 4 crud

--- src/University/University.Application/UseCases/SubjectsCase/Dtos/SubjectsDto.cs
+++ src/University/University.Application/UseCases/SubjectsCase/Dtos/SubjectsDto.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.SubjectsCase.Dtos
 {
     public class SubjectsDto
     {
         public string SubjectName { get; set; }
+        public List<int> Subjects { get; set; }
     }
 }
