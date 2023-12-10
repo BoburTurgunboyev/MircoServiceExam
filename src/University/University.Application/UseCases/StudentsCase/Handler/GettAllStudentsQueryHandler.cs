4edeeb5face839db6456ecaaffc9e36a79c1cb80
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:37:45 +0500

    project 4 crud

--- src/University/University.Application/UseCases/StudentsCase/Handler/GettAllStudentsQueryHandler.cs
+++ src/University/University.Application/UseCases/StudentsCase/Handler/GettAllStudentsQueryHandler.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.StudentsCase.Queries;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.StudentsCase.Handler
 {
-    internal class GettAllStudentsQueryHandler
+    public class GettAllStudentsQueryHandler : IRequestHandler<GettAllStudentsQuery, List<Students>>
     {
+        private readonly IAppDbConection _appDbConection;
+
+        public GettAllStudentsQueryHandler(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<List<Students>> Handle(GettAllStudentsQuery request, CancellationToken cancellationToken)
+        {
+            var res =await _appDbConection.studentss.ToListAsync(cancellationToken);
+            return res;
+        }
     }
 }
