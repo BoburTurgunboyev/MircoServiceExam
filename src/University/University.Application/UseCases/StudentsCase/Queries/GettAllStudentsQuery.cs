f97ef39789bddb77e8b1f652571a262e294c752d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:37:26 +0500

    project 4 crud

--- src/University/University.Application/UseCases/StudentsCase/Queries/GettAllStudentsQuery.cs
+++ src/University/University.Application/UseCases/StudentsCase/Queries/GettAllStudentsQuery.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.StudentsCase.Queries
 {
-    internal class GettAllStudentsQuery
+    public class GettAllStudentsQuery : IRequest<List<Students>>
     {
     }
 }
