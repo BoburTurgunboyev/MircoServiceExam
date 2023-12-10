41cc679deca061c9fdb1019406bf0425889e12bd
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:42:13 +0500

    project 4 crud

---
+++ src/EduCentr/EduCentr.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using EduCentr.Application.Absreaction;
+using EduCentr.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentr.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IAppDbConnect, AppDbConnent>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnections"));
+            });
+
+            return services;
+        }
+    }
+}
