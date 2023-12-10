834db411c549bb029c86b25a44610ada6b62c9a0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:40:24 +0500

    project 4 crud

---
+++ src/University/University.Api/Controllers/ExamResultsControllers/ExamResultsController.cs
@@ -0,0 +1,74 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using University.Application.UseCases.ExamResultsCase.Commands;
+using University.Application.UseCases.ExamResultsCase.Dtos;
+using University.Application.UseCases.ExamResultsCase.Queries;
+
+namespace University.Api.Controllers.ExamResultsControllers
+{
+    [Route("api/[controller][action]")]
+    [ApiController]
+    public class ExamResultsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExamResultsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateExamResults(ExemResultsDto exemResultsDto)
+        {
+            var exem = new CreateExamResultsCommand()
+            {
+
+                ExemMark = exemResultsDto.ExemMark,
+                Terms = exemResultsDto.Terms,
+                Year = exemResultsDto.Year,
+                StudentSubjectId = exemResultsDto.StudentSubjectId,
+
+
+            };
+            var res = await _mediator.Send(exem);
+            return Ok("Cretaed");
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllExamResults()
+        {
+            var result = await _mediator.Send(new GetAllExamResultsQuery());
+            return Ok(result);
+        }
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetByIdExamResults(int id)
+        {
+            var result = await _mediator.Send(new GetByIdExamResultsQuery() { Id = id });
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateExamResults(int id, ExemResultsDto exemResultsDto)
+        {
+            var exem = new UpdateExemResultsCommand()
+            {
+                ExemMark =exemResultsDto.ExemMark,
+                Terms=exemResultsDto.Terms,
+                Year = exemResultsDto.Year,
+                StudentSubjectId=exemResultsDto.StudentSubjectId,
+            };
+
+            var res = await _mediator.Send(exem);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteExamResults(int id)
+        {
+            var result = await _mediator.Send(new DeleteExamResultsCommand() { Id = id });
+            return Ok("Deleted");
+        }
+    }
+}
---
+++ src/University/University.Api/Controllers/OverAllMarksControllers/OverAllMarksController.cs
@@ -0,0 +1,72 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using University.Application.UseCases.ExamResultsCase.Commands;
+using University.Application.UseCases.ExamResultsCase.Dtos;
+using University.Application.UseCases.ExamResultsCase.Queries;
+using University.Application.UseCases.OverAllMarksCase.Commands;
+using University.Application.UseCases.OverAllMarksCase.Dtos;
+using University.Application.UseCases.OverAllMarksCase.Queries;
+
+namespace University.Api.Controllers.OverAllMarksControllers
+{
+    [Route("api/[controller][action]")]
+    [ApiController]
+    public class OverAllMarksController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OverAllMarksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+
+        public async ValueTask<IActionResult> CreateOverAllMarks(OverAllMarksDto overAllMarksDto)
+        {
+            var mark = new CreateOverAllMarksCommand()
+            {
+
+              StudentsId = overAllMarksDto.StudentsId,  
+              MarkType = overAllMarksDto.MarkType,
+
+            };
+            var res = await _mediator.Send(mark);
+            return Ok("Cretaed");
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllOverAllMarks()
+        {
+            var result = await _mediator.Send(new GetAllOverAllMarksQuery());
+            return Ok(result);
+        }
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetByIdOverAllMarks(int id)
+        {
+            var result = await _mediator.Send(new GetByIdOverAllMarksQuery() { Id = id });
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateOverAllMarks(int id, OverAllMarksDto overAllMarksDto)
+        {
+            var mark = new UpdateOverAllMarksCommand()
+            {
+                StudentsId= overAllMarksDto.StudentsId,
+                MarkType= overAllMarksDto.MarkType,
+            };
+
+            var res = await _mediator.Send(mark);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteOverAllMarks(int id)
+        {
+            var result = await _mediator.Send(new DeleteOverAllMarkscommand() { Id = id });
+            return Ok("Deleted");
+        }
+    }
+}
