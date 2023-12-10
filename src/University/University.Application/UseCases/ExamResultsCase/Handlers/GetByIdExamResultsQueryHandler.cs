65dc1e19ec1a57881381e45a5b44fb37302f2ed7
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:26 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/ExamResultsCase/Handlers/GetByIdExamResultsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.ExamResultsCase.Queries;
+using University.Domain.Entities;
+
+namespace University.Application.UseCases.ExamResultsCase.Handlers
+{
+    public class GetByIdExamResultsQueryHandler : IRequestHandler<GetByIdExamResultsQuery, ExamResults>
+    {
+        private readonly IAppDbConection _appDbConection;
+
+        public GetByIdExamResultsQueryHandler(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<ExamResults> Handle(GetByIdExamResultsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbConection.examResultss.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
