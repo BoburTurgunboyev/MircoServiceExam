0aefc6b961451bc49dac0ef7e24ba36f26afcede
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:42:26 +0500

    project 4 crud

---
+++ src/EduCentr/EduCentr.Infrastructure/Configrations/EduStudentConfigration.cs
@@ -0,0 +1,30 @@
+﻿using EduCentr.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentr.Infrastructure.Configrations
+{
+    public class EduStudentConfigration : IEntityTypeConfiguration<EduStudent>
+    {
+        public void Configure(EntityTypeBuilder<EduStudent> entity)
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Firstname).IsRequired();
+            entity.Property(e => e.Lastname).IsRequired();
+            entity.Property(e => e.Age).IsRequired();
+            entity.Property(e => e.Phone).IsRequired();
+            entity.Property(e => e.Email).IsRequired();
+
+            entity.HasMany(e => e.Categories)
+                .WithMany(c => c.EduStudents)
+                .UsingEntity(nameof(CategoryEduStudent));
+                
+                
+        }
+    }
+}
