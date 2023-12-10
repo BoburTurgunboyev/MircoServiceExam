cf049a99b1ff73a1be6fcff28db45e5139e6ee57
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:51 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/ExamResultsCase/Commands/DeleteExamResultsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.UseCases.ExamResultsCase.Commands
+{
+    public class DeleteExamResultsCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
