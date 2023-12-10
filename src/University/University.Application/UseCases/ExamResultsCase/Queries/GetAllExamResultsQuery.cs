cd34d7e3d56c10b3219d24ae9d1663ec8f77d1b9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:16 +0500

    project 4 crud

--- src/University/University.Application/UseCases/ExamResultsCase/Queries/GetAllExamResultsQuery.cs
+++ src/University/University.Application/UseCases/ExamResultsCase/Queries/GetAllExamResultsQuery.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.ExamResultsCase.Queries
 {
-    internal class GetAllExamResultsQuery
+    public class GetAllExamResultsQuery : IRequest<List<ExamResults>>
     {
     }
 }
