78695ae0460f802523e0dd8456f1982edcc53217
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:38:23 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/StudentsCase/Commands/DeleteStudentsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.UseCases.StudentsCase.Commands
+{
+    public class DeleteStudentsCommand : IRequest<bool>
+    {
+        public int Id { get; set; } 
+    }
+}
