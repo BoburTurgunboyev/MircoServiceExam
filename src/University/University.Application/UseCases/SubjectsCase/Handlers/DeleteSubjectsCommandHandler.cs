7d49bac2a0de27c9a9b03434146cdbfc7b7bb4bc
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:36:29 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/SubjectsCase/Handlers/DeleteSubjectsCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.SubjectsCase.Commands;
+
+namespace University.Application.UseCases.SubjectsCase.Handlers
+{
+    public class DeleteSubjectsCommandHandler : IRequestHandler<DeleteSubjectsCommand, bool>
+    {
+        private readonly IAppDbConection _appDbConection;
+
+        public DeleteSubjectsCommandHandler(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<bool> Handle(DeleteSubjectsCommand request, CancellationToken cancellationToken)
+        {
+            var res = await _appDbConection.subjectss.FirstOrDefaultAsync(x =>x.Id == request.Id);
+            if (res == null)
+            {
+                return false;
+            }
+            _appDbConection.subjectss.Remove(res);
+            var result =await _appDbConection.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
