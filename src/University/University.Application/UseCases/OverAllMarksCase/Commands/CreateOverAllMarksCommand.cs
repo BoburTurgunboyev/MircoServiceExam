b0ad4a9b9da92c2f4c7ccccff1e8ed3f83b5012e
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:07 +0500

    project 4 crud

--- src/University/University.Application/UseCases/OverAllMarksCase/Commands/CreateOverAllMarksCommand.cs
+++ src/University/University.Application/UseCases/OverAllMarksCase/Commands/CreateOverAllMarksCommand.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.UseCases.OverAllMarksCase.Dtos;
 
 namespace University.Application.UseCases.OverAllMarksCase.Commands
 {
-    internal class CreateOverAllMarksCommand
+    public class CreateOverAllMarksCommand : OverAllMarksDto, IRequest<bool>
     {
     }
 }
