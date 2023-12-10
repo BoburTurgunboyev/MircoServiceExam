c7b18c46042fd7816f5a985dbf314a6d1a99a47f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:38:48 +0500

    project 4 crud

--- src/University/University.Application/UseCases/OverAllMarksCase/Handlers/CreateOverAllMarksCommandHandler.cs
+++ src/University/University.Application/UseCases/OverAllMarksCase/Handlers/CreateOverAllMarksCommandHandler.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.OverAllMarksCase.Commands;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.OverAllMarksCase.Handlers
 {
-    internal class CreateOverAllMarksCommandHandler
+    public class CreateOverAllMarksCommandHandlerv : IRequestHandler<CreateOverAllMarksCommand, bool>
     {
+        private readonly IAppDbConection _appDbConection;
+
+        public CreateOverAllMarksCommandHandlerv(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<bool> Handle(CreateOverAllMarksCommand request, CancellationToken cancellationToken)
+        {
+            var OverAllMarks = new OverAllMarks()
+            {
+                MarkType =request.MarkType,
+                StudentsId = request.StudentsId,
+
+            };
+
+            _appDbConection.overAllMarkss.Add(OverAllMarks);
+            var result = await _appDbConection.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
     }
 }
