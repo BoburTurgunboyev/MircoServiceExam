ef182e2be9186d72b32926ca9151c9ecb11e4aa9
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:39:11 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/ExamResultsCase/Queries/GetByIdExamResultsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Domain.Entities;
+
+namespace University.Application.UseCases.ExamResultsCase.Queries
+{
+    public class GetByIdExamResultsQuery : IRequest<ExamResults>
+    {
+        public int Id { get; set; }
+    }
+}
