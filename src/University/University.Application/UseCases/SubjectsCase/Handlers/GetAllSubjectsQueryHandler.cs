260f2b2f467203e76cb98f0e4812dd517525c95f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:36:24 +0500

    project 4 crud

--- src/University/University.Application/UseCases/SubjectsCase/Handlers/GetAllSubjectsQueryHandler.cs
+++ src/University/University.Application/UseCases/SubjectsCase/Handlers/GetAllSubjectsQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using University.Application.Absreactions;
+using University.Application.UseCases.SubjectsCase.Queries;
+using University.Domain.Entities;
 
 namespace University.Application.UseCases.SubjectsCase.Handlers
 {
-    internal class GetAllSubjectsQueryHandler
+    public class GetAllSubjectsQueryHandler : IRequestHandler<GetAllSubjectsQuery, List<Subjects>>
     {
+        private readonly IAppDbConection _appDbConection;
+
+        public GetAllSubjectsQueryHandler(IAppDbConection appDbConection)
+        {
+            _appDbConection = appDbConection;
+        }
+
+        public async Task<List<Subjects>> Handle(GetAllSubjectsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _appDbConection.subjectss.ToListAsync(cancellationToken);
+            return result;
+        }
     }
 }
