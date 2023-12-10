92163ee2da722bf1813e03369cb804d28ce24f68
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-10 15:38:58 +0500

    project 4 crud

---
+++ src/University/University.Application/UseCases/OverAllMarksCase/Commands/UpdateOverAllMarksCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.Application.UseCases.OverAllMarksCase.Dtos;
+
+namespace University.Application.UseCases.OverAllMarksCase.Commands
+{
+    public class UpdateOverAllMarksCommand : OverAllMarksDto,IRequest<bool>
+    {
+        public int Id { get; set; } 
+    }
+}
